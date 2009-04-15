f58ad7be0505a1d0158fb64df1781c7a7e846ebb
Fabien Schwob <fabien@x-phuture.com>
2009-04-15 22:19:43 +0200

    Added admin actions to mark blog post as published/draft

--- admin.py
+++ admin.py
@@ -2,6 +2,24 @@ from django.contrib import admin
 from blogging.models import Category, Post
 from attachements.admin import AttachementInline
 
+def make_published(modeladmin, request, queryset):
+    rows_updated = queryset.update(status=Post.PUBLISHED)
+    if rows_updated == 1:
+        message_bit = "1 post was"
+    else:
+        message_bit = "%s posts were" % rows_updated
+    modeladmin.message_user(request, "%s successfully marked as published." % message_bit)
+make_published.short_description = "Mark selected stories as published"
+
+def make_draft(modeladmin, request, queryset):
+    rows_updated = queryset.update(status=Post.DRAFT)
+    if rows_updated == 1:
+        message_bit = "1 post was"
+    else:
+        message_bit = "%s posts were" % rows_updated
+    modeladmin.message_user(request, "%s successfully marked as draft." % message_bit)
+make_draft.short_description = "Mark selected stories as draft"
+
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
     list_display = ('name','site')
@@ -17,4 +35,5 @@ class PostAdmin(admin.ModelAdmin):
     inlines = [
         AttachementInline,
     ]
+    actions = [make_published, make_draft]
 admin.site.register(Post, PostAdmin)
