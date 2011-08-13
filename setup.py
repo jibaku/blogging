25ac0258fad0c789583d741ea2ae0f3589033db8
Fabien Schwob <github@x-phuture.com>
2011-08-13 20:44:29 +0200

    Improving setup and version management

--- setup.py
+++ setup.py
@@ -1,16 +1,36 @@
 from distutils.core import setup
- 
+import os
+
+package = "blogging"
+version = __import__('blogging').get_version().replace(' ', '-')
+
+def read_file(filename):
+    """Read a file into a string"""
+    path = os.path.abspath(os.path.dirname(__file__))
+    filepath = os.path.join(path, filename)
+    try:
+        return open(filepath).read()
+    except IOError:
+        return ''
+
+def get_readme():
+    """Return the README file contents. Supports text,rst, and markdown"""
+    for name in ('README','README.rst','README.md'):
+        if os.path.exists(name):
+            return read_file(name)
+    return ''
+
 setup(
-    name = "blogging",
-    version = "0.2",
+    name = package,
+    version = version,
     author = "Fabien Schwob",
     author_email = "fabien@x-phuture.com",
     description = "Yet another django blogging app",
-    long_description = "Yet another django blogging app",
+    long_description = get_readme(),
     license = "BSD",
-    url = "https://hg.jibaku.net/modules/blogging/",
+    url = "http://github.com/jibaku/blogging",
     packages = [
-        "blogging",
+        package,
     ],
     classifiers = [
         "Development Status :: 3 - Alpha",
