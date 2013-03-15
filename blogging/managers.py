804a23d1836035ba2c7b7a61b227948a5ca9ad07
Fabien Schwob <github@x-phuture.com>
2013-03-15 15:35:20 +0100

    * Making blogging time-zone aware (and formatted blogging/managers.py for PEP8)

--- blogging/managers.py
+++ blogging/managers.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
-
-import datetime
-
 from django.db import models
 from django.conf import settings
+from django.utils import timezone
+
 
 # Category related
 class AvailableCategoriesManager(models.Manager):
@@ -15,26 +14,27 @@ class AvailableCategoriesManager(models.Manager):
     def get_by_natural_key(self, slug, site_id):
         return self.get(slug=slug, site__id=site_id)
 
+
 # Post related
 class PostManager(models.Manager):
     def published(self):
-        now = datetime.datetime.now()
+        now = timezone.now()
         queryset = self.filter(published_on__lte=now)
         queryset = queryset.filter(status=self.model.PUBLISHED)
         return queryset
-    
+
     def related_items(self, item):
         """
         Return items related to the item passed as parameter
         """
         return self.availables.published().filter(categories__in=item.categories.all()).exclude(id=item.id).distinct()[:5]
 
+
 class AvailableItemsManager(PostManager):
     def get_query_set(self):
         queryset = super(AvailableItemsManager, self).get_query_set()
         queryset = queryset.filter(site__id__exact=settings.SITE_ID)
         return queryset
-    
+
     def get_by_natural_key(self, slug, site_id):
         return self.get(slug=slug, site__id=site_id)
-
