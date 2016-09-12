4886a935f8af86c7a72a804038590ec3df3621bb
Fabien Schwob <fabien.schwob@89grad.ch>
2016-09-12 22:24:00 +0200

    Making code compatible with Django 1.10. Now need at least Django>=1.8

--- blogging/urls/__init__.py
+++ blogging/urls/__init__.py
@@ -1,11 +1,11 @@
-from django.conf.urls import patterns, url, include
+from django.conf.urls import url
 
 
 from blogging.views import PostDetailView
 from blogging.urls.base import base_patterns
 
-urlpatterns = patterns('',)
+urlpatterns = []
 urlpatterns += base_patterns
-urlpatterns += patterns('',
+urlpatterns += [
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d+)/(?P<slug>[-\w]+)$', PostDetailView.as_view(), name="blog-item"),
-)
+]
--- blogging/urls/yearmonth.py
+++ blogging/urls/yearmonth.py
@@ -4,8 +4,8 @@ from django.conf.urls import patterns, url, include
 from blogging.views import PostDetailView
 from blogging.urls.base import base_patterns
 
-urlpatterns = patterns('',)
+urlpatterns = []
 urlpatterns += base_patterns
-urlpatterns += patterns('',
+urlpatterns += [
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<slug>[-\w]+)$', PostDetailView.as_view(), name="blog-item"),
-)
+]
