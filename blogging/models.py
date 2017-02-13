1879fe6170c2dc8091f7d3dc333295b18464765b
Fabien Schwob <fabien.schwob@89grad.ch>
2017-02-13 09:41:40 +0100

    Making models.py more compatible with py3 and py2

--- blogging/models.py
+++ blogging/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-import logging
+from __future__ import unicode_literals
+
 import hashlib
 import time
 
@@ -7,18 +8,17 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from django.contrib.sites.managers import CurrentSiteManager
 from django.contrib.sites.models import Site
-from django.core.urlresolvers import reverse, NoReverseMatch
+from django.core.urlresolvers import NoReverseMatch, reverse
 from django.db import models
-from django.db.models.signals import post_save
+from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext as _
-import django.dispatch
 
-from blogging.managers import AvailableCategoriesManager
-from blogging.managers import AvailableItemsManager
-from blogging.managers import PostManager
+from blogging.managers import (AvailableCategoriesManager,
+                               AvailableItemsManager, PostManager)
 
 
 def upload_to_blogging(instance, filename):
+    """Create file path."""
     hasher = hashlib.md5()
     hasher.update(filename.encode('utf-8'))
     hasher.update(str(time.time()))
@@ -29,30 +29,32 @@ def upload_to_blogging(instance, filename):
     )
 
 
+@python_2_unicode_compatible
 class Picture(models.Model):
-    name = models.CharField(_(u"Name"), max_length=100)
-    slug = models.SlugField(_(u"Slug"))
-    description = models.TextField(_(u"Description"), blank=True)
+    name = models.CharField(_("Name"), max_length=100)
+    slug = models.SlugField(_("Slug"))
+    description = models.TextField(_("Description"), blank=True)
     site = models.ForeignKey(Site, verbose_name=_("Site"), default=settings.SITE_ID)
     image = models.ImageField(upload_to=upload_to_blogging, max_length=200)
 
-    def __unicode__(self):
+    def __str__(self):
+        """Human picture name."""
         return self.name
 
 
+@python_2_unicode_compatible
 class Category(models.Model):
-    """
-    A category to regroup similar articles
-    """
-    name = models.CharField(_(u"Name"), max_length=100)
-    slug = models.SlugField(_(u"Slug"))
-    description = models.TextField(_(u"Description"), blank=True)
+    """A category to regroup similar articles."""
+
+    name = models.CharField(_("Name"), max_length=100)
+    slug = models.SlugField(_("Slug"))
+    description = models.TextField(_("Description"), blank=True)
     site = models.ForeignKey(Site, verbose_name=_("Site"), default=settings.SITE_ID)
     picture = models.ForeignKey(Picture, verbose_name=_("Picture"), blank=True, null=True)
 
     # hidden cached field
-    visible_posts_count = models.IntegerField(_(u"Visible posts in category"), editable=False, default=0)
-    all_posts_count = models.IntegerField(_(u"All posts in category"), editable=False, default=0)
+    visible_posts_count = models.IntegerField(_("Visible posts in category"), editable=False, default=0)
+    all_posts_count = models.IntegerField(_("All posts in category"), editable=False, default=0)
 
     objects = models.Manager()  # The default manager.
     on_site = CurrentSiteManager()
@@ -60,10 +62,11 @@ class Category(models.Model):
 
     class Meta:
         ordering = ['name']
-        verbose_name = _(u"category")
-        verbose_name_plural = _(u"categories")
+        verbose_name = _("category")
+        verbose_name_plural = _("categories")
 
-    def __unicode__(self):
+    def __str__(self):
+        """Human category name."""
         return self.name
 
     def get_absolute_url(self):
@@ -89,6 +92,7 @@ class Category(models.Model):
         self.save()
 
 
+@python_2_unicode_compatible
 class Post(models.Model):
     """
     The Post contains the generic fields for a blog item, like the publication
@@ -99,29 +103,29 @@ class Post(models.Model):
     PUBLISHED = 2
     DELETED = 3
     STATUS_CHOICES = (
-        (DRAFT, _(u'Draft')),
-        (PUBLISHED, _(u'Published')),
-        (DELETED, _(u'Deleted')),
+        (DRAFT, _("Draft")),
+        (PUBLISHED, _("Published")),
+        (DELETED, _("Deleted")),
     )
-    
-    title = models.CharField(_(u"Title"), max_length=150)
-    slug = models.SlugField(_(u"Slug"), unique=True, max_length=150, db_index=True)
-    author = models.ForeignKey(User, verbose_name=_(u"Author"))
-    excerpt = models.TextField(_(u"Excerpt"), blank=True, db_column="exceprt")
-    content = models.TextField(_(u"Content"))
+
+    title = models.CharField(_("Title"), max_length=150)
+    slug = models.SlugField(_("Slug"), unique=True, max_length=150, db_index=True)
+    author = models.ForeignKey(User, verbose_name=_("Author"))
+    excerpt = models.TextField(_("Excerpt"), blank=True, db_column="exceprt")
+    content = models.TextField(_("Content"))
     main_picture = models.ForeignKey(Picture, verbose_name=_("Picture"), blank=True, null=True)
 
-    published_on = models.DateTimeField(_(u"Published on"), db_index=True)
+    published_on = models.DateTimeField(_("Published on"), db_index=True)
     created_on = models.DateTimeField(auto_now_add=True, editable=False)
     updated_on = models.DateTimeField(auto_now=True, editable=False)
-    status = models.IntegerField(_(u"Status"), choices=STATUS_CHOICES, db_index=True, default=DRAFT)
-    
-    selected = models.BooleanField(_(u"Selected"), default=False)
-    comments_open = models.BooleanField(_(u"Are comments open?"), default=True)
+    status = models.IntegerField(_("Status"), choices=STATUS_CHOICES, db_index=True, default=DRAFT)
 
-    categories = models.ManyToManyField(Category, verbose_name=_(u"Categories"))
+    selected = models.BooleanField(_("Selected"), default=False)
+    comments_open = models.BooleanField(_("Are comments open?"), default=True)
 
-    site = models.ForeignKey(Site, verbose_name=_(u"Site"), default=settings.SITE_ID)
+    categories = models.ManyToManyField(Category, verbose_name=_("Categories"))
+
+    site = models.ForeignKey(Site, verbose_name=_("Site"), default=settings.SITE_ID)
 
     # Managers
     objects = PostManager()
@@ -130,10 +134,11 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-published_on']
-        verbose_name = _(u"item")
-        verbose_name_plural = _(u"items")
+        verbose_name = _("item")
+        verbose_name_plural = _("items")
 
-    def __unicode__(self):
+    def __str__(self):
+        """Human post name."""
         return self.title
 
     def natural_key(self):
@@ -163,13 +168,9 @@ class Post(models.Model):
                 return reverse('blog-item', kwargs=kwargs, urlconf=settings.ROOT_URLCONF)
 
     def __item_cache_key(self):
-        """
-        Return a unique item key that can be used in order to cache it
-        """
+        """Return a unique item key that can be used in order to cache it."""
         return "blogging:post:{0}".format(self.id)
 
     def related_items(self):
-        """
-        Return items related to the current item
-        """
+        """Return items related to the current item."""
         return Post.availables.related_items(self)
